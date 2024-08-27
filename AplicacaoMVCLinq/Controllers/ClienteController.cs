179a5e37c2799e7bcb5739cef9f9ad29df1c6ec8
Rayssa <rayssa_nan.svl@hotmail.com>
2024-08-27 16:36:17 -0300

    Finalizando a lista de clientes

--- AplicacaoMVCLinq/Controllers/ClienteController.cs
+++ AplicacaoMVCLinq/Controllers/ClienteController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AplicacaoMVCLinq.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AplicacaoMVCLinq.Controllers
 {
@@ -7,6 +8,24 @@ namespace AplicacaoMVCLinq.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult GetClientes()
+        {
+            Cliente c1 = new Cliente(1, "Alice", "alice@example.com", "1234-5678", new DateOnly(2000, 6, 1));
+            Cliente c2 = new Cliente(2, "Alice", "alice@example.com", "1234-5678", new DateOnly(2000, 6, 1));
+            Cliente c3 = new Cliente(3, "Alice", "alice@example.com", "1234-5678", new DateOnly(2000, 6, 1));
+            Cliente c4 = new Cliente(4, "Alice", "alice@example.com", "1234-5678", new DateOnly(2000, 6, 1));
+            Cliente c5 = new Cliente(5, "Alice", "alice@example.com", "1234-5678", new DateOnly(2000, 6, 1));
+
+            List<Cliente> listaClientes = new List<Cliente>();
+            listaClientes.Add(c1);
+            listaClientes.Add(c2);
+            listaClientes.Add(c3);
+            listaClientes.Add(c4);
+            listaClientes.Add(c5);
+
+            return View(listaClientes);
         }
     }
 }
--- AplicacaoMVCLinq/Models/Cliente.cs
+++ AplicacaoMVCLinq/Models/Cliente.cs
@@ -2,10 +2,19 @@
 {
     public class Cliente
     {
-        public int Id { get; set; }
+        public int ClienteId { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
         public string Telefone { get; set; }
         public DateOnly DataNasc { get; set; }
+
+        public Cliente(int id, string nome, string email, string telefone, DateOnly dataNasc)
+        {
+            this.ClienteId = id;
+            this.Nome = nome;
+            this.Email = email;
+            this.Telefone = telefone;
+            this.DataNasc = dataNasc;
+        }
     }
 }
